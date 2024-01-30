01ac26d7c15b255a746fa511a5bc710896fd7688
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-01-29 22:05:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return "This one is on me!";
+  } else if (ride >= 401 && ride <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (ride > 2000 && ride <= 2049) {
+    return "I will gladly take your thirty bucks.";
+  } else if (ride > 2050) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+    return 'Thank you so much.';
+    case 'not as generous':
+    return 'Thank you.';
+    default:
+    return 'Bye.';
+    }
 }
